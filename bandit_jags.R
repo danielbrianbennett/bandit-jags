24fb95afa78d7e99a86f6ffd29423c8c50bfbeac
Daniel Bennett <danielbrianbennett@gmail.com>
2016-07-04 16:18:47 +1000

    First working model
    - Made model work
    - Hierarchically extended model

--- bandit_jags.R
+++ bandit_jags.R
@@ -1,26 +1,29 @@
 # clear workspace:  
-rm(list=ls()) 
+rm(list=ls())
+
+# load rjags package
+library(R2jags)
 
 # set a couple of helpful variables
-nParticipants <- 10 # if less than the full number, we will take a subset of participants to test code
 nTrials <- 30 # per block
 nBlocks <- 3
 nBandits <- 4
-
+mean0 <- 0
+variance0 <- 1000
+fudgeFactor <- .00001
 
 # define working directory and datafile
 dataDir <- "~/Google Drive/Works in Progress/JSBANDIT/Bandit/data/Bandit project shared data/"
 dataFile <- "banditData_v2point2.RData"
+bugsFile <- "~/Documents/Git/bandit-jags/bandit_jags.txt"
 
 # set working directory and load file
 setwd(dataDir)
 load(dataFile)
 
-# load rjags package
-library(R2jags)
-
-# extract the first 10 participants
-subsetID <- unique(sorted.data$ID)[1:10]
+# extract participant data
+nParticipants <- 3#length(unique(sorted.data$ID)) # how many participants' data to test?
+subsetID <- unique(sorted.data$ID)[1:nParticipants]
 extract <- subset(sorted.data, ID %in% subsetID)
 
 # recode choices from string to numeric
@@ -30,27 +33,44 @@ extract$choice[extract$choice == "bottom"] = 3
 extract$choice[extract$choice == "left"] = 4
 extract$choice <- as.numeric(extract$choice)
 
-# create data containers to pass to jags, and fill them
-d <- array(data = NA, dim = c(nBlocks,nTrials,nParticipants)) # choice data
-points <- array(data = NA, dim = c(nBlocks,nTrials,nParticipants)) # points data
+extract$whichFilled[extract$whichFilled == "none"] = 0
+extract$whichFilled[extract$whichFilled == "top"] = 1
+extract$whichFilled[extract$whichFilled == "right"] = 2
+extract$whichFilled[extract$whichFilled == "bottom"] = 3
+extract$whichFilled[extract$whichFilled == "left"] = 4
+extract$whichFilled <- as.numeric(extract$whichFilled)
 
+# create data containers to pass to jags
+choices <- array(data = NA, dim = c(nBlocks,nTrials,nParticipants)) # choice data
+points <- array(data = NA, dim = c(nBlocks,nTrials,nParticipants)) # points data
+changeLag <- array(data = NA, dim = c(nBlocks,nTrials,nParticipants)) # change lag data
+whichFilled <- array(data = NA, dim = c(nBlocks,nTrials,nParticipants)) # change lag data
+
+# deal data to arrays built above
 for (p in 1:nParticipants){
-  d[,,p] <- matrix(extract[extract$ID == subsetID[p],]$choice,nBlocks,nTrials,byrow = T)
+  choices[,,p] <- matrix(extract[extract$ID == subsetID[p],]$choice,nBlocks,nTrials,byrow = T)
   points[,,p] <- matrix(extract[extract$ID == subsetID[p],]$pointsWon,nBlocks,nTrials,byrow = T)
+  changeLag[,,p] <- matrix(extract[extract$ID == subsetID[p],]$changeLag,nBlocks,nTrials,byrow = T)
+  whichFilled[,,p] <- matrix(extract[extract$ID == subsetID[p],]$whichFilled,nBlocks,nTrials,byrow = T)
 }
 
 # list data to be passed on to JAGS
-data <- list("nTrials",
+data <- list("choices",
+             "points",
+             "changeLag",
+             "whichFilled",
+             "nBandits", 
              "nBlocks",
+             "nTrials",
              "nParticipants",
-             "nBandits",
-             "d", 
-             "points"
-             ) 
+             "mean0",
+             "variance0",
+             "fudgeFactor"
+) 
 
 # list parameters to estimate in JAGS
 parameters <- c("sigma_zeta",
-                "sigma_gamma",
+                "sigma_epsilon",
                 "mu_q",
                 "sigma_q",
                 "mu_p",
@@ -61,17 +81,18 @@ parameters <- c("sigma_zeta",
 
 # initial values of parameters
 initVals <-	list(list(
-            sigma_zeta = 10,
-            sigma_gamma = 10,
-            mu_q = 5,
-            sigma_q = 10,
-            mu_p = 5,
-            sigma_p = 10,
-            mu_b = 2,
-            sigma_b = 5
+            sigma_zeta = 4,
+            sigma_epsilon = 24,
+            mu_q = 1,
+            sigma_q = .2,
+            mu_p = 1,
+            sigma_p = .2,
+            mu_b = 4,
+            sigma_b = 1
             ))
 
 # call jags
-samples <- jags(data, inits=initVals, parameters, model.file = "bandit_jags.txt", 
-                n.chains=1, n.iter=5000, n.burnin=2000, n.thin=1)
-
+ptm <- proc.time()
+samples <- jags(data, inits=initVals, parameters, model.file = bugsFile, 
+                n.chains=1, n.iter=5000, n.burnin=2000, n.thin=2)
+proc.time() - ptm
--- bandit_jags_single.R
+++ bandit_jags_single.R
@@ -9,6 +9,9 @@ participantID <- 10308915 # which participant's data to test?
 nTrials <- 30 # per block
 nBlocks <- 3
 nBandits <- 4
+mean0 <- 0
+variance0 <- 1000
+fudgeFactor <- .00001
 
 # define working directory and datafile
 dataDir <- "~/Google Drive/Works in Progress/JSBANDIT/Bandit/data/Bandit project shared data/"
@@ -30,22 +33,36 @@ extract$choice[extract$choice == "bottom"] = 3
 extract$choice[extract$choice == "left"] = 4
 extract$choice <- as.numeric(extract$choice)
 
+extract$whichFilled[extract$whichFilled == "none"] = 0
+extract$whichFilled[extract$whichFilled == "top"] = 1
+extract$whichFilled[extract$whichFilled == "right"] = 2
+extract$whichFilled[extract$whichFilled == "bottom"] = 3
+extract$whichFilled[extract$whichFilled == "left"] = 4
+extract$whichFilled <- as.numeric(extract$whichFilled)
+
+# recode negative change lags
+extract$changeLag[extract$changeLag < 0] <- fudgeFactor
+
 # create data containers to pass to jags, and fill them
 choices <- array(data = NA, dim = c(nBlocks,nTrials)) # choice data
 points <- array(data = NA, dim = c(nBlocks,nTrials)) # points data
+changeLag <- matrix(extract[extract$ID == subsetID,]$changeLag,nBlocks,nTrials,byrow = T) # record how long since change
 choices[,] <- matrix(extract[extract$ID == subsetID,]$choice,nBlocks,nTrials,byrow = T)
 points[,] <- matrix(extract[extract$ID == subsetID,]$pointsWon,nBlocks,nTrials,byrow = T)
-mean0 = 0
-variance0 = 1000
+whichFilled <- matrix(extract[extract$ID == subsetID,]$whichFilled,nBlocks,nTrials,byrow = T) 
+
 
 # list data to be passed on to JAGS
 data <- list("choices",
+             "changeLag",
              "nBandits", 
              "nBlocks",
              "nTrials",
              "points",
+             "whichFilled",
              "mean0",
-             "variance0"
+             "variance0",
+             "fudgeFactor"
 ) 
 
 # list parameters to estimate in JAGS
@@ -60,12 +77,11 @@ parameters <- c("sigma_zeta",
 initVals <-	list(list(
   sigma_zeta = 10,
   sigma_epsilon = 10,
-  b = 5,
-  p = 5,
-  q = 10
+  b = 10,
+  p = .1,
+  q = .1
 ))
 
 # call jags
 samples <- jags(data, inits=initVals, parameters, model.file = bugsFile, 
                 n.chains=1, n.iter=5000, n.burnin=2000, n.thin=1)
-
